ccf58fd8ca9461bfd37fe58eca66b17e956490f9
Ayrton Vitorino <Ayrton100sp@gmail.com>
2021-05-27 18:32:13 -0300

    att

---
+++ ExerciciosJava/PooGuanabara/src/aula11polimorfismosobrescrever/Ave.java
@@ -0,0 +1,31 @@
+package aula11polimorfismosobrescrever;
+
+public class Ave extends Animal{
+    private String corPena;
+
+    public void fazerNinho(){
+        System.out.println("Construindo Ninho");
+    }
+    @Override
+    public void locomover() {
+        System.out.println("Voando");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo Frutas");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de ave");
+    }
+
+    public String getCorPena() {
+        return corPena;
+    }
+
+    public void setCorPena(String corPena) {
+        this.corPena = corPena;
+    }
+}
--- ExerciciosJava/PooGuanabara/src/aula11polimorfismosobrescrever/Chamada.java
+++ ExerciciosJava/PooGuanabara/src/aula11polimorfismosobrescrever/Chamada.java
@@ -5,11 +5,16 @@ public class Chamada {
         Mamifero ma[] = new Mamifero[1];
         Reptil ra[] = new Reptil[1];
         Peixe pe[] = new Peixe[1];
+        Ave av[] = new  Ave[1];
 
         ma[0] = new Mamifero();
         ra[0] = new Reptil();
         pe[0] = new Peixe();
+        av[0] = new Ave();
 
+        ma[0].setCorPelo("Marrom");
+        ma[0].setPeso(35.5f);
+        ma[0].setIdade(4);
         ma[0].locomover();
         ma[0].alimentar();
         ma[0].emitirSom();
